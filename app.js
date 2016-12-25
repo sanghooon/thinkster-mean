61c6e6d4b8c8774d2a76a28fd0ce9a2f629e6f84
Sanghoon Han <sanghooon@yahoo.com>
2016-12-25 18:42:13 -0500

    Finished basic tutorial

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
-var app = angular.module('flapperNews', []);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
 
-app.controller('MainCtrl', [
-  '$scope',
-  function($scope) {
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
-    $scope.addPost = function() {
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0
-      });
-      $scope.title = '';
-      $scope.link = '';
-    };
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    };
-  }]);
+require('./models/Posts');
+require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+
+var app = express();
+
+mongoose.connect('mongodb://localhost/news');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
